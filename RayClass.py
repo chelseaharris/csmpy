9fe184fd9e7d249750ee05941c3028b789e718b7
Chelsea Harris <chelseaharris@berkeley.edu>
2017-09-23 09:10:13 -0500

    RayClass: added function for merging cells (degrading model resolution

--- RayClass.py
+++ RayClass.py
@@ -8,12 +8,60 @@ import glob
 import Const           as C
 import ChevKasen_tools as CKtools
 
+def calc_av(arr):
+    return np.mean(arr)
+
+
+def merge_cells(hi_ray, k, tol, lo_ray, i):
+    # hi_ray: high resolution ray we're degrading
+    # k     : index in the high resolution ray that we're on
+    # tol   : fractional difference tolerance in density and temperature
+    # lo_ray: low resolution ray we're writing into
+    # i     : index in the low resolution ray that we're on
+    GAM = 5./3
+
+    Q1 = hi_ray.rho[k]
+    d1 = abs(hi_ray.rho[k:] - Q1)/Q1
+    cand = np.where( d1 > tol )[0]
+    k_m1 = hi_ray.size()-k-1 if len(cand)==0 else cand[0]
+
+    Q2 = hi_ray.T_gas[k]
+    d2 = abs(hi_ray.T_gas[k:] - Q2)/Q2
+    cand = np.where(  d2 > tol )[0]
+    k_m2 = hi_ray.size()-k-1 if len(cand)==0 else cand[0]
+
+    k_m = min(k_m1,k_m2) + k
+
+    kslice = slice(k,k_m) if k_m>k else k
+
+    lo_ray.r1[i] = hi_ray.r1[k]
+    lo_ray.r2[i] = hi_ray.r2[k_m]
+
+    lo_ray.rho  [i] = calc_av(hi_ray.rho  [kslice])
+    lo_ray.T_gas[i] = calc_av(hi_ray.T_gas[kslice])
+    lo_ray.v    [i] = calc_av(hi_ray.v    [kslice])
+
+    p_gas = C.K_B/C.M_P*lo_ray.rho[i]*lo_ray.T_gas[i]
+    lo_ray.p_gas[i] = p_gas
+    # u_gas = p_gas/(gam_fac - 1)
+    lo_ray.u_gas[i] = p_gas/(GAM - 1)
+    # sqrt(gamfac_ * grid->z[i].p_gas/grid->z[i].rho)
+    lo_ray.cs   [i] = np.sqrt( GAM * p_gas/lo_ray.rho[i] )
+
+    # if we're not at the end of lo_ray or hi_ray, keep merging
+    knext = k_m if k_m > k else k+1
+    if i+1 < lo_ray.size() and knext < hi_ray.size():
+        i = merge_cells(hi_ray, knext, tol, lo_ray, i+1)
+
+    return i
+        
+
 class Ray(object):
     """
     Member variables of this class: time, r1, r2, r, rho, T_gas, p_gas, v, u_gas, cs, n_e
     """
 
-    def __init__(self, a_props, a_time=-1, a_n_e = None, a_n_I = None, u_per_cc = False, a_r=[]):
+    def __init__(self, a_props, a_time=-1, a_n_e = [], a_n_I = [], u_per_cc = False, a_r=[]):
         """
         INPUTS
         a_props  : 2D array of properties. Rows are
@@ -85,8 +133,8 @@ class Ray(object):
     def __getitem__(self,i):
         props = [ self.r1[i], self.r2[i], self.rho[i], self.T_gas[i], self.p_gas[i], self.v[i], self.u_gas[i], self.cs[i] ]
         return Ray( props, self.time, 
-                    (self.n_e[i] if self.n_e!=None else None), 
-                    (self.n_I[i] if self.n_I!=None else None), 
+                    (self.n_e[i] if len(self.n_e)>0 else []), 
+                    (self.n_I[i] if len(self.n_I)>0 else []), 
                     True, self.r[i] )
 
 
@@ -141,6 +189,20 @@ class Ray(object):
             return self.cell_mass(i1)
         else:
             return sum( [ self.cell_mass(i) for i in range(i1,i2) ] );
+
+
+    def calc_N_e(self, i=[]):
+        if i==[]:
+            i = range(len(self.r1))
+        return sum(self.n_e[i] * (self.r2[i] - self.r1[i]))
+
+    def calc_N(self, i=[]):
+        if i==[]:
+            i = range(len(self.r1))
+
+        n = self.n_e[i] + self.n_I[i]
+        dr = self.r2[i] - self.r1[i]
+        return sum(n * dr)
 
 
     def cell_at_v(self,vel,tol=-1):
@@ -201,16 +263,16 @@ class Ray(object):
         return 4*np.pi/3 * (self.r2[i1]**3 - self.r1[i0]**3)
 
 
-    def set_n_e(self, a_n_e=None):
-        if a_n_e == None:
+    def set_n_e(self, a_n_e=[]):
+        if len(a_n_e) == 0:
             self.n_e = self.rho / C.M_P # fully ionized hydrogen
         else:
             assert len(a_n_e) == len(self.r1)
             self.n_e = a_n_e
 
 
-    def set_n_I(self, a_n_I=None):
-        if a_n_I == None:
+    def set_n_I(self, a_n_I=[]):
+        if len(a_n_I) == 0:
             self.n_I = self.rho / C.M_P  # fully ionized hydrogen
         else:
             assert len(a_n_I) == len(self.r1)
@@ -222,6 +284,77 @@ class Ray(object):
 
         return self.n_e + self.n_I
 
+
+    def write_as_initmod(self, outname='WOOOOOO.mod'):
+        if self.time<0: 
+            print('Can\'t write the ray until it has a legit positive time in seconds.')
+            return self.time
+
+        header = "{:d} {:d} {:d} {:f} {:e} {:d}\n".format(1, self.size(), 1, 0.1, self.time, 1)
+        warr = np.vstack((self.r1, self.r2, self.rho, self.p_gas, self.v, self.v, np.zeros(self.size()))).T # What is it good for? 
+        with open(outname, 'w') as wf:
+            np.savetxt(wf, warr, header=header, comments='')
+        
+
+    def degrade(self, N_lowres, tol):
+        # Initialize low resolution array
+        low_props = np.zeros((8,N_lowres))
+        low_props[1] = np.arange(N_lowres) + 1
+        low_r = np.zeros(N_lowres)
+        low_ray = Ray(low_props, a_r = low_r)
+    
+        # Populate low-res ray with merged hi-res cells
+        i_last = merge_cells(self, 0, tol, low_ray, 0)
+
+        low_ray = low_ray[:i_last]
+        # Let me know if I gave too many cells
+        if i_last+1 < N_lowres:
+            print('Only needed {} cells of the {} alotted'.format(i_last+1, N_lowres))
+        
+        # Let me know if I gave too few cells
+        dr = abs(low_ray.r2[-1] - self.r2[-1])/self.r2[-1]
+        if dr>1e-16: 
+            print('could not capture full ray -- increase tol or N_lowres, or trim the ray -- dr={:e}'.format(dr))
+    
+        # Calculate the average radius of low-res cells
+        low_ray.r = CKtools.calc_zone_r(low_ray.r1, low_ray.r2)
+    
+        return low_ray
+
+    def blind_degrade(self, N_lowres):
+        # Initialize low resolution array
+        low_props = np.zeros((8,N_lowres))
+        low_props[1] = np.arange(N_lowres) + 1
+        low_r = np.zeros(N_lowres)
+        lo_ray = Ray(low_props, a_r = low_r)
+    
+        # Populate low-res ray with merged hi-res cells
+        GAM = 5./3
+    
+        k_bin = np.linspace(0,self.size()-1,lo_ray.size()+1,dtype=int)
+
+        for i in range(lo_ray.size()):
+            k = slice(k_bin[i], k_bin[i+1])
+        
+            lo_ray.r1[i] = self.r1[k_bin[i  ]]
+            lo_ray.r2[i] = self.r2[k_bin[i+1]]
+        
+            lo_ray.rho  [i] = calc_av(self.rho  [k])
+            lo_ray.T_gas[i] = calc_av(self.T_gas[k])
+            lo_ray.v    [i] = calc_av(self.v    [k])
+        
+            p_gas = C.K_B/C.M_P*lo_ray.rho[i]*lo_ray.T_gas[i]
+            lo_ray.p_gas[i] = p_gas
+            # u_gas = p_gas/(gam_fac - 1)
+            lo_ray.u_gas[i] = p_gas/(GAM - 1)
+            # sqrt(gamfac_ * grid->z[i].p_gas/grid->z[i].rho)
+            lo_ray.cs   [i] = np.sqrt( GAM * p_gas/lo_ray.rho[i] )
+    
+        # Calculate the average radius of low-res cells
+        lo_ray.r = CKtools.calc_zone_r(lo_ray.r1, lo_ray.r2)
+    
+        return lo_ray
+    
 
 class Cell(object):
     """
