7479aa88ef7d3a76860c16d3b6c74ff28865d5e7
Chelsea Harris <chelseaharris@berkeley.edu>
2018-02-27 09:06:54 -0800

    added more constants to Const.py

--- Const.py
+++ Const.py
@@ -16,9 +16,10 @@ SIGMA_TH  = (8*PI/3) * E_ESU**4 / (M_E**2 * C_LIGHT**4); # Thomson cross-section
 M_CH      = 1.4*M_SUN;                                   # non-rotating Chandrasekhar mass
 GAMMA_AD  = 5./3;                                        # ideal gas adiabatic coefficient
 SIGMA_TOT = PI*E_ESU**2 / (M_E*C_LIGHT)                  # atomic line profile nomalization [cm^2 Hz]
+RYD_EN    = 2.17992e-11            # Rydberg energy/H ionization energy (erg) from Allen AQ
 # Physical constants in other units
-M_P_GEV   = 0.9382720462814463    # ((C.m_p * C.c**2).to(u.GeV)).value;
-M_E_ERG   = M_E * C_LIGHT**2 ;
+M_P_GEV   = 0.9382720462814463     # proton mass in GeV ((C.m_p * C.c**2).to(u.GeV)).value;
+M_E_ERG   = M_E * C_LIGHT**2 ;     # electron mass in erg
 # Unit conversions
 # ... time
 YR2SEC    = 31557600.0 # ((1*u.yr).to(u.s)).value;
@@ -32,7 +33,8 @@ ERG2EV    = 624150934326.018 # ( (1*u.erg).to(u.eV ) ).value;
 EV2ERG    = 1./ERG2EV
 HZ2ERG    = 6.62606957e-27   # ( (C.h*(1*u.Hz)).to(u.erg) ).value;
 HZ2EV     = HZ2ERG * ERG2EV;
-MET2ERG   = HZ2ERG * C_LIGHT/(1e2)
+EV2HZ     = 1/HZ2EV;
+MET2ERG   = HZ2ERG * C_LIGHT/(1e2) # meters to erg
 # ... length
 ANG2CM    = 1e-8
 KM2CM     = 1e5
@@ -41,11 +43,12 @@ PC2CM     = 3.08567758149137e18
 
 # More Complicated Conversions
 # ... wavelength/frequency
-Ang2Hz      = lambda lm_Ang: C_LIGHT/(lm_Ang*ANG2CM)
+Ang2Hz      = lambda lm_Ang: C_LIGHT/(lm_Ang*ANG2CM) [::-1]
 Ang2erg     = lambda lm_Ang: H*Ang2Hz(lm_Ang)
 # ... specific fluxes or luminosities
 F_Ang2F_Hz  = lambda lm_Ang, f_Ang: lm_Ang**2/C_LIGHT * f_Ang * ANG2CM # (f_Ang/ANG2CM) * (lm_Ang*ANG2CM)**2
 F_Ang2F_erg = lambda lm_Ang, f_Ang: F_Ang2F_Hz(lm_Ang, f_Ang) / H
+MJY2CGS = 1e-26
 # ... times
 def TimeConvert( a_time, a_u_now, a_u_des ):
     Time_to_Sec = {'yr'  : YR2SEC, 
